049dbca21cd3aa9e14bb2f1aedea9dec5e39d0d8
moloq4 <mukimako7@gmail.com>
2024-12-05 20:24:34 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+LOL - komik bir şeye verilen cevap
+CRINGE - garip ya da utandırıcı bir şey
+ROFL - bir şakaya karşılık cevap
+SHEESH - onaylamamak
+CREEPY - korkunç
+AGGRO - agresifleşmek/sinirlenmek
+
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+if word in meme_dict.keys():
+    # Kelime eşleşiyorsa ne yapmalıyız?
+else:
+    # Kelime eşleşmiyorsa ne yapmalıyız?
